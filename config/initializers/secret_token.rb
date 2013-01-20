000a4baa4621fed57b3ebf33fef99ddb3371c138
Anton Ignashev <ignashev@gmail.com>
2013-01-20 15:05:20 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NikaclubRu::Application.config.secret_token = 'ef70ea6530f07ed9931118530572a7b22bc3ec666d41630ffe1a32315956954c7d58d24b81a421e122f8405f0ca2a63baa3a8f44c24ff8e0358557b617a2bd11'
